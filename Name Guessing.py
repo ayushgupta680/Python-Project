861be8f5daa4cc73ef4e8353849a962d439da51c
Ayush Gupta <ayushgupta680@gmail.com>
2026-03-22 09:26:34 +0530

    random

--- Name Guessing.py
+++ Name Guessing.py
@@ -1,7 +1,19 @@
-def add(a,d):
-    return a+b
+import random
 
-for i in range(3):
-    a = int(input("Enter the numbers:"))
-    b = int(input("Enter the numbers:"))
-    print(add(a,b))
+number = random.randint(1, 100)
+guess = 0
+
+while guess != number:
+    guess = int(input("Enter the numbers:"))
+    
+    if guess < number:
+        print("Too Low")
+    elif guess > number:
+        print("Too Hight")
+    else:
+        print("Correct")
+        
+1
+        
+
+
